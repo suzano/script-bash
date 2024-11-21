65a8d00ca36172c189dc9094871c2936f7813dcf
Suzano Bitencourt <suzanobitencourt@gmail.com>
2024-11-21 18:21:36 -0300

    Script - debian_update.sh

--- debian_update.sh
+++ debian_update.sh
@@ -36,7 +36,8 @@
 ################################################################################
 
 # 1. Verifica se existe conexao com a internet
-echo "Verificando conexão com a Internet..."
+clear
+echo "1. Verificando conexão com a Internet..."
 ping -c 2 google.com &> /dev/null #¹
 if [ $? -eq 0 ]; then #²
     echo "Conexão com a Internet estável."
@@ -44,57 +45,64 @@ else
     echo "Sem conexão com a Internet. Verifique a rede e tente novamente."
     exit 1
 fi
+echo ""
 
 # 2. Atualiza o repositorio
-echo "Atualizando repositórios..."
+echo "2. Atualizando repositórios..."
 sudo apt update -y
 if [ $? -ne 0 ]; then
     echo "Erro ao atualizar repositórios. Verifique a configuração do apt."
     exit 1
 fi
+echo ""
 
 # 3. Repara pacotes quebrados
-echo "Reparando pacotes quebrados..."
+echo "3. Reparando pacotes quebrados..."
 sudo dpkg --configure -a
 if [ $? -ne 0 ]; then
     echo "Erro ao reparar pacotes quebrados. Tente usar o comando: sudo dpkg --force-remove-reinstreq --remove <pacote>."
     exit 1
 fi
+echo ""
 
 # 4. Atualiza o sistema
-echo "Atualizando o sistema..."
+echo "4. Atualizando o sistema..."
 sudo apt upgrade -y
 if [ $? -ne 0 ]; then
     echo "Erro ao atualizar o sistema. Verifique a configuração ou os pacotes instalados."
     exit 1
 fi
+echo ""
 
 # 5. Remove pacotes baixados pelo APT
-echo "Removendos todos os pacotes baixados pelo APT..."
+echo "5. Removendos todos os pacotes baixados pelo APT..."
 sudo apt clean -y
 if [ $? -ne 0 ]; then
     echo "Erro ao remover pacotes baixados pelo APT."
     exit 1
 fi
+echo ""
 
 # 6. Remove pacotes que não tiveram seu download concluído
-echo "Removendo pacotes incompletos..."
+echo "6. Removendo pacotes incompletos..."
 sudo apt autoclean -y
 if [ $? -ne 0 ]; then
     echo "Erro ao remover pacotes incompletos."
     exit 1
 fi
+echo ""
 
 # 7. Remove dependências que não são mais necessárias pelo sistema
-echo "Removendo dependências que não são mais necessárias pelo sistema..."
+echo "7. Removendo dependências que não são mais necessárias pelo sistema..."
 sudo apt autoremove -y
 if [ $? -ne 0 ]; then
     echo "Erro ao remover dependências que não são mais necessárias pelo sistema."
     exit 1
 fi
+echo ""
 
 # 8. Reinicia o sistema
-echo "Reiniciando o sistema em 10 segundos. Pressione Ctrl+C para cancelar."
+echo "8. Reiniciando o sistema em 10 segundos. Pressione Ctrl+C para cancelar."
 sleep 10
 sudo reboot
 
