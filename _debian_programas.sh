65a76d00ca3b179812ce8521be198fdcb5f84fc3
Suzano Bitencourt <suzanobitencourt@gmail.com>
2025-03-27 15:48:12 -0300

    Script - debian_programas.sh

--- _debian_programas.sh
+++ _debian_programas.sh
@@ -107,5 +107,20 @@ echo "8. Reiniciando o sistema em 10 segundos. Pressione Ctrl+C para cancelar."
 sleep 10
 sudo reboot
 
+# Add the AnyDesk GPG key
+sudo apt update
+sudo apt install ca-certificates curl apt-transport-https
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://keys.anydesk.com/repos/DEB-GPG-KEY -o /etc/apt/keyrings/keys.anydesk.com.asc
+sudo chmod a+r /etc/apt/keyrings/keys.anydesk.com.asc
+
+# Add the AnyDesk apt repository
+echo "deb [signed-by=/etc/apt/keyrings/keys.anydesk.com.asc] https://deb.anydesk.com all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list > /dev/null
+
+# Update apt caches and install the AnyDesk client
+sudo apt update
+sudo apt install anydesk
+
+
 #¹ O comando "&>/dev/null" redireciona a saída e os erros de um programa para o arquivo especial "/dev/null", que descarta intencionalmente qualquer dado enviado para ele.
 #² A variável "$?" armazena o status de saída do último comando executado.
